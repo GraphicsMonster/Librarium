b9741103c006c3fb9adf8e012b0230a1541a5e85
Devansh Gupta <63590510+GraphicsMonster@users.noreply.github.com>
2023-05-19 19:56:18 +0530

    Additional states defined in the library contract

--- Smart contracts/library.sol
+++ Smart contracts/library.sol
@@ -3,4 +3,18 @@ pragma solidity >=0.4.16 <0.9.0;
 
 import "./bookToken.sol";
 
-contract Library {}
+contract Library {
+    address private bookTokenAddress;
+    bookToken private bookTokenContract;
+
+    constructor(address _bookTokenAddress) {
+        bookTokenAddress = _bookTokenAddress;
+        bookTokenContract = bookToken(bookTokenAddress);
+    }
+
+    mapping(address => uint256) public bookBalance;
+    mapping(address => mapping(uint256 => uint256)) public booksIssued;
+
+    event bookBorrowed(address indexed _borrower, uint256 indexed _bookId);
+    event bookReturned(address indexed _borrower, uint256 indexed _bookId);
+}
