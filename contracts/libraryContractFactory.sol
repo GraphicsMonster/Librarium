5d300d8d38fb2c796e5b7f0a12af8eb82ad03fba
Devansh Gupta <63590510+GraphicsMonster@users.noreply.github.com>
2023-06-04 17:27:39 +0530

    lib address indexed to a unique library id

--- contracts/libraryContractFactory.sol
+++ contracts/libraryContractFactory.sol
@@ -4,24 +4,29 @@ pragma solidity >=0.4.16 <0.9.0;
 import "./Library.sol";
 
 contract LibraryContractFactory {
-    mapping(address => address) public libraries;
+    mapping(uint256 => address) public libraries;
+    // Maps a unique integer to the library address
+
+    uint256 public totalLibraries = 0;
+    // Keeps track of the total number of libraries created by this contract
 
     event LibraryCreated(
         address indexed libraryAddress,
-        address indexed customerAddress
+        uint256 indexed libraryId
     );
 
     function createLibrary() public {
         // Deploy a new instance of the Library contract
         Library newLibrary = new Library(10);
-        // Store the library address mapped to the customer address
-        libraries[msg.sender] = address(newLibrary);
+        // Store the library address mapped to its unique address in this contract
+        totalLibraries = totalLibraries + 1;
+        libraries[totalLibraries] = address(newLibrary);
         // Emit an event to notify the creation of a new library
-        emit LibraryCreated(address(newLibrary), msg.sender);
+        emit LibraryCreated(address(newLibrary), totalLibraries);
     }
 
-    function getLibrary() public view returns (address) {
+    function getLibrary(uint256 library_id) public view returns (address) {
         // Retrieve the library address for the calling customer
-        return libraries[msg.sender];
+        return libraries[library_id];
     }
 }
