4a9ade171938ccd02d6205f8cea821cc78b6bbfd
Saksham Gupta <www.rameshgupta67@gmail.com>
2023-06-16 12:28:57 +0530

    user registration api call set up

--- client/librarium/src/pages/UserRegistration/UserRegistration.jsx
+++ client/librarium/src/pages/UserRegistration/UserRegistration.jsx
@@ -1,23 +1,56 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import {useParams} from 'react-router-dom';
 import './UserRegistration.css';
 
 const user_credentials = {
-  username: '',
+  address: '',
+  name: '',
   email: '',
+};
+
+const db_user_credentials = {
+  username: '',
   password: ''
 };
 
-const onSubmit = () => {
+const onSubmit = async () => {
   user_credentials.username = document.getElementById('username').value;
   user_credentials.email = document.getElementById('user-email').value;
   user_credentials.password = document.getElementById('password').value;
+
+  await sendData();
 };
 
 const getCredentials = () => {
   // This function will return the user credentials
   return user_credentials;
 };
+
+const sendData = async () => {
+  try {
+    const {id} = useParams();
+    const url = `http://localhost:3000/api/library/${id}/registeruser`
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(getCredentials())
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+    }
+    else {
+      console.log('Failed to register user');
+    }
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
 
 const UserRegistration = () => {
   return (
@@ -27,6 +60,20 @@ const UserRegistration = () => {
           <div className='col-25 user-username'>Username</div>
           <div className='col-75 user-input'>
             <input type='text' id='username' name='username' placeholder='Enter Username' />
+          </div>
+        </div>
+
+        <div className='row'>
+          <div className='col-25 user-name'>Name</div>
+          <div className='col-75 user-input'>
+            <input type='text' id='name' name='name' placeholder='Enter Name' />
+          </div>
+        </div>
+
+        <div className='row'>
+          <div className='col-25 user-address'>Ethereum Address</div>
+          <div className='col-75 user-input'>
+            <input type='text' id='address' name='address' placeholder='Enter your Ethereum Address' />
           </div>
         </div>
 
